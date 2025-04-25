41f833cc5e030fa10f6406cc22df2c7eb566e2d0
schatten96 <gocryemokid96@gmail.com>
2025-04-25 23:16:00 +0400

    [infrastructure-1] Added CI build conf
    - also added clang-format check
    - fixed example formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <boost/asio.hpp>
 #include <iostream>
 
-int main() {
+int
+main() {
     boost::asio::io_context io;
     boost::asio::steady_timer timer(io, boost::asio::chrono::seconds(2));
 
